94b104ac38ec6bec6e5980cbcd50184a17596a1f
myh_sx <amshanxin@gmail.com>
2023-05-21 21:06:09 +0800

    springboot练习

---
+++ src/test/java/com/sx/TliasWebManagementApplicationTests.java
@@ -0,0 +1,33 @@
+package com.sx;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest
+class TliasWebManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void genJwt(){
+        Map<String,Object> claims = new HashMap<>();
+        claims.put("id",1);
+        claims.put("username","Tom");
+
+        String jwt = Jwts.builder()
+                .setClaims(claims) //自定义内容(载荷)
+                .signWith(SignatureAlgorithm.HS256, "itheima") //签名算法
+                .setExpiration(new Date(System.currentTimeMillis() + 24*3600*1000)) //有效期
+                .compact();
+
+        System.out.println(jwt);
+    }
+}
